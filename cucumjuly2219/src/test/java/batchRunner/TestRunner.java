a4a95a4823eb92d40ce7da696cc9f4c1c2fc4c2a
adhithyaakella <adhithya003@gmail.com>
2019-07-24 18:12:51 +0530

    new code+

--- cucumjuly2219/src/test/java/batchRunner/TestRunner.java
+++ cucumjuly2219/src/test/java/batchRunner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 
 
-@RunWith(Cucumber .class)
+@RunWith(Cucumber.class)
 @CucumberOptions(features="FeaFolder",glue="stepDef22july",tags= {"@Smoke"},
 plugin={"html:target/cucumber-htmlreport.html"})
 public class TestRunner {
